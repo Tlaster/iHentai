0006b44bc4db58b145092f97c9048804706e6cf7
Tlaster <tlaster@outlook.com>
2017-10-17 11:55:55 +0800

    Remove Fuel (replace with Flurl)

---
+++ src/iHentai/Services/iHentai.Services.Core/FlurlExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Flurl.Http;
+using Html2Model;
+
+namespace iHentai.Services.Core
+{
+    internal static class FlurlExtension
+    {
+        public static async Task<T> ReceiveHtml<T>(this Task<HttpResponseMessage> response) where T: class, new()
+        {
+            return HtmlConvert.DeserializeObject<T>(await response.ReceiveString());
+        }
+    }
+}
