e84c73294af010ad6fe32d0b96e659594e642720
Tlaster <Tlaster@outlook.com>
2017-10-17 09:50:12 +0800

    add fuel (not completed)

---
+++ src/iHentai/iHentai.Shared/Fuel/FuelRequest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace iHentai.Core.iHentai.Shared.Fuel
+{
+    public class FuelRequest<T>
+    {
+
+        public FuelRequest(string uri, IDictionary<string, T> data)
+        {
+            Uri = uri;
+            Data = data;
+        }
+
+        public string Uri { get; }
+
+        public HttpMethod Method { get; set; }
+
+        public IDictionary<string, T> Data { get; set; }
+
+        public IDictionary<string, string> Cookie { get; set; } = new Dictionary<string, string>();
+
+        public IDictionary<string, string> Header { get; set; } = new Dictionary<string, string>();
+
+        public PostTypes PostType { get; set; }
+    }
+    public enum PostTypes
+    {
+        MultipartFormData,
+        FormUrlEncoded
+    }
+}
