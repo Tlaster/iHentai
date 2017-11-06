876c1a356d2577ca68e2fd6d5de9bc2f3cd3aa62
Tlaster <tlaster@outlook.com>
2017-11-06 12:23:45 +0800

    Create ExListViewRenderer for MacOS

---
+++ src/iHentai/Platforms/iHentai.MacOS/Renderers/ExListViewRenderer.cs
@@ -0,0 +1,21 @@
+﻿using AppKit;
+using CoreGraphics;
+using ObjCRuntime;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace iHentai.MacOS.Renderers
+{
+    public class ExListViewRenderer : ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+        }
+
+        public override void ScrollWheel(NSEvent theEvent)
+        {
+            base.ScrollWheel(theEvent);
+        }
+    }
+}
