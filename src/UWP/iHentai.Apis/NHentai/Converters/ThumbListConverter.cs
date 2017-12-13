4947337a858f07c00b089201d2a8a08e6fd3efb5
Tlaster <tlaster@outlook.com>
2017-12-13 18:34:27 +0800

    Fix

--- src/UWP/iHentai.Apis/NHentai/Converters/ThumbListConverter.cs
+++ src/UWP/iHentai.Apis/NHentai/Converters/ThumbListConverter.cs
@@ -18,7 +18,7 @@ namespace iHentai.Apis.NHentai.Converters
             return Enumerable.Range(0, model.Images.Pages.Length).Select(item => new
             {
                 Link = $"https://t.nhentai.net/galleries/{model.MediaId}/{item}t.{(string.Equals(model.Images.Pages[item].Type, "j", StringComparison.OrdinalIgnoreCase) ? "jpg" : "png")}",
-                Page = item
+                Page = item + 1
             });
         }
 
