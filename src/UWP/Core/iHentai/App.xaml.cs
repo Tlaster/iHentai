98b67bc7f66893a972fc0ffe131db036c618f5d7
Tlaster <tlaster@outlook.com>
2017-12-31 23:21:33 +0800

    Last commit from 2017

--- src/UWP/Core/iHentai/App.xaml.cs
+++ src/UWP/Core/iHentai/App.xaml.cs
@@ -22,7 +22,6 @@ namespace iHentai
         public App()
         {
             InitializeComponent();
-
             // Deferred execution until used. Check https://msdn.microsoft.com/library/dd642331(v=vs.110).aspx for further info on Lazy<T> class.
             _activationService = new Lazy<ActivationService>(CreateActivationService);
         }
