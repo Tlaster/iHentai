35c29b63a1e5f134514eab1c98679daeeb03b021
Tlaster <tlaster@outlook.com>
2018-03-23 19:38:25 +0800

    Tweaks

---
+++ src/UWP/Conet/Conet.Mastodon/Models/InstanceData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iHentai.Services;
+
+namespace Conet.Mastodon.Models
+{
+    public class InstanceData : IInstanceData
+    {
+        public string AccessToken { get; set; }
+        public string Uid { get; set; }
+        public string Domain { get; set; }
+    }
+}
--- src/UWP/Conet/Conet.Mastodon/ViewModels/AccountSelectionViewModel.cs
+++ src/UWP/Conet/Conet.Mastodon/ViewModels/AccountSelectionViewModel.cs
@@ -11,6 +11,7 @@ namespace Conet.Mastodon.ViewModels
     [Startup]
     public class AccountSelectionViewModel : ViewModel
     {
+
         public AccountSelectionViewModel()
         {
             
