5e5a3cce24259a80f9080268f16700b44913bb66
Tlaster <tlaster@outlook.com>
2019-11-24 23:25:32 +0800

    Init files

---
+++ UWP/iHentai/iHentai/RootView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using iHentai.Common;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace iHentai
+{
+    public sealed partial class RootView
+    {
+        public RootView()
+        {
+            this.InitializeComponent();
+            CoreApplication.GetCurrentView().TitleBar.Also(it =>
+            {
+                it.ExtendViewIntoTitleBar = true;
+                it.LayoutMetricsChanged += OnCoreTitleBarOnLayoutMetricsChanged;
+            });
+            Window.Current.SetTitleBar(TitleBar);
+            ApplicationView.GetForCurrentView().TitleBar.Also(it =>
+            {
+                it.ButtonBackgroundColor = Colors.Transparent;
+                it.ButtonInactiveBackgroundColor = Colors.Transparent;
+            });
+        }
+
+        private void OnCoreTitleBarOnLayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            TitleBar.Height = sender.Height;
+        }
+    }
+}
