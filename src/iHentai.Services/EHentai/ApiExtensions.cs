43588a2a6ba40378f0e4bff9fef182002e60a9a9
Tlaster <tlaster@outlook.com>
2020-05-21 16:02:40 +0800

    Fix build

--- src/iHentai.Services/EHentai/ApiExtensions.cs
+++ src/iHentai.Services/EHentai/ApiExtensions.cs
@@ -9,23 +9,23 @@ namespace iHentai.Services.EHentai
 {
     internal static class ApiExtensions
     {
-        public static Task<T> GetHtmlAsync<T>(
+        public static Task<T?> GetHtmlAsync<T>(
             this Url url,
             CancellationToken cancellationToken = default,
-            HttpCompletionOption completionOption = HttpCompletionOption.ResponseContentRead)
+            HttpCompletionOption completionOption = HttpCompletionOption.ResponseContentRead) where T: class
         {
             return new FlurlRequest(url).GetHtmlAsync<T>(cancellationToken, completionOption);
         }
 
-        public static Task<T> GetHtmlAsync<T>(
+        public static Task<T?> GetHtmlAsync<T>(
             this IFlurlRequest request,
             CancellationToken cancellationToken = default,
-            HttpCompletionOption completionOption = HttpCompletionOption.ResponseContentRead)
+            HttpCompletionOption completionOption = HttpCompletionOption.ResponseContentRead) where T: class
         {
             return request.SendAsync(HttpMethod.Get, null, cancellationToken, completionOption).ReceiveHtml<T>();
         }
 
-        public static async Task<T> ReceiveHtml<T>(this Task<IFlurlResponse> response)
+        public static async Task<T?> ReceiveHtml<T>(this Task<IFlurlResponse> response) where T: class
         {
             using var resp = await response;
             if (resp == null)
