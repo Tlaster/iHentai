f67f4d9456339818de6e49e34ad80012de02c2a2
Tlaster <tlaster@outlook.com>
2020-10-03 01:34:38 +0800

    time to remove old source

--- src-old/iHentai.Services/Core/IPreferences.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace iHentai.Services.Core
-{
-    public interface IPreferences
-    {
-        T Get<T>(string key, T defaultValue = default);
-        void Set<T>(string key, T value);
-    }
-}
